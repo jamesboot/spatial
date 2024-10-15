fae43c542c0457c361b5b6e5dfee69f13098ad39
jamesboot <james.boot.94@gmail.com>
2024-10-15 11:19:09 +0100

    Edit Data_Prep.sh to work with only essential data downloaded

--- CosMx_Data_Prep.sh
+++ CosMx_Data_Prep.sh
@@ -1,21 +1,21 @@
-# Note that this code works for a single slide. If exporting
-# multiple slides, some modification might be necessary.
-
-# cd to parent directory
+# Note that this code works for a single slide.
+# The name of the slide needs to be the first input
+# To run the script, cd to the directory containing RawFiles and flatFiles directories 
 
 # create a directory adjacent to RawData and flatFiles
-mkdir -p sample_dir_formatted && cd $_
+mkdir -p sample_dir_formatted/$1 && cd $_
 
 # Add flat files
-for file in $(ls ../flatFiles/*/*csv*); do cp $file ./; done
+for file in $(ls ../../flatFiles/$1/*csv*); do cp $file ./; done
 
-# Add folders
-cp -r ../RawFiles/*/*/CellStatsDir/CellComposite ./
-cp -r ../RawFiles/*/*/CellStatsDir/CellOverlay ./
+# Copy folders
+cp -r ../../RawFiles/$1/CellComposite ./
+cp -r ../../RawFiles/$1/CellOverlay ./
 
+# Extract CellLabels from each FOV folder
 mkdir -p CellLabels
-for file in $(ls ../RawFiles/*/*/CellStatsDir/FOV*/CellLabels*); do cp $file ./CellLabels/ ; done
-rm ./CellLabels/._Cell*
-  
+for file in $(ls ../../RawFiles/$1/FOV*/CellLabels*); do cp $file ./CellLabels/ ; done
+
+# Extract CompartmentLabels from each FOV folder 
 mkdir -p CompartmentLabels
-for file in $(ls ../RawFiles/*/*/CellStatsDir/FOV*/CompartmentLabels*); do cp $file ./CompartmentLabels/ ; done
+for file in $(ls ../../RawFiles/$1/FOV*/CompartmentLabels*); do cp $file ./CompartmentLabels/ ; done
